909ba9656104e311d4f912ccecffd13b533733d1
Thiago Bezerra <thiagobezerra.dev@gmail.com>
2024-10-14 00:54:27 +0000

    configurações e padronização do desenvolvimento do projeto com devcontainers

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,2 @@
+sudo apt-get update && export DEBIAN_FRONTEND=noninteractive \
+     && sudo apt-get -y install --no-install-recommends clang clangd cmake libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libxkbcommon-dev wayland-protocols libwayland-dev libgl1-mesa-dev xorg-dev
